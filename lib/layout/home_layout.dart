9c0761a809e30b0b83df8d1e2c76c8d368f865e7
ahmed3id <ahmed3idahmed@gmail.com>
2023-06-03 19:15:47 +0300

    scaffold BG

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../models/SourceResponse.dart';
-import '../models/Sources.dart';
 import '../screens/home_screen.dart';
-import '../shared/remote/api_manager.dart';
 
 class HomeLayout extends StatefulWidget {
   static const String routeName = "HomeLayout";
@@ -15,23 +11,30 @@ class HomeLayout extends StatefulWidget {
 class _HomeLayoutState extends State<HomeLayout> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      drawer: Drawer(
-        child: Container(color: Colors.red,width: 180),
-      ),
-      appBar: AppBar(
-        toolbarHeight: 100,
-        title: Text("News App"),
-        centerTitle: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('assets/images/pattern copy.jpg'),
+        fit: BoxFit.cover,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        drawer: Drawer(
+          child: Container(color: Colors.red, width: 180),
+        ),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(103, 172, 92, 0.8),
+          toolbarHeight: 100,
+          title: Text("News App"),
+          centerTitle: true,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(50),
             bottomRight: Radius.circular(50),
-          )
+          )),
         ),
+        body: HomeScreen(),
       ),
-      body: HomeScreen(),
     );
   }
 }
